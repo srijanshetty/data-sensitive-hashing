a3b178b04a76aa63adde2fd0208367d2ad0830ea
Srijan R Shetty <srijan.shetty@gmail.com>
2015-04-18 10:24:12 +0530

    Indentation and comment for myrandom

--- myrandom.h
+++ myrandom.h
@@ -2,11 +2,12 @@
 #define MYRANDOM_H_INCLUDED
 
 #include <stdlib.h>
-class MyRandom{
+
+class MyRandom {
     public:
-    static int int_random(int);
-    static void rand_multi_gaussian(float [], int);
-    static float rand_single_gaussian();
+        static int int_random(int);
+        static void rand_multi_gaussian(float [], int);
+        static float rand_single_gaussian();
 };
 
 #endif // MYRANDOM_H_INCLUDED
