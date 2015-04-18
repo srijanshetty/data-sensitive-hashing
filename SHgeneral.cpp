8d41f427b36c4e6a9d003d2c6ccc2e6c737619d5
Srijan R Shetty <srijan.shetty@gmail.com>
2015-04-18 10:36:41 +0530

    Indentation and comment for SHgeneral

--- SHgeneral.cpp
+++ SHgeneral.cpp
@@ -23,15 +23,18 @@ SHGeneral::SHGeneral()
 
 void SHGeneral::init()
 {
-	if(isinit)return;
+	if(isinit) return;
     isinit = true;
 	cout<<"SHGeneral::init"<<endl;
-	family_generator();
-	generate_hashtableindex();
-	productcomputer();
+	family_generator();                     // Generate random familtyvectors
+	generate_hashtableindex();              // Create the concatenation functions by sampling familyvectors
+	productcomputer();                      // Compute product of data and familyvectors
 	cout<<"SHGeneral::init--END"<<endl;
 }
 
+/**
+  * Intitalizes each famliyvector with a random multi gaussian
+  */
 void SHGeneral::family_generator()
 {
     cout<<"SHGeneral::family_generator"<<endl;
@@ -47,6 +50,11 @@ void SHGeneral::family_generator()
     cout<<"SHGeneral::family_generator--END"<<endl;
 }
 
+/**
+  * Each hashtable is a concatenation function of 'M' random hash family functions,
+  * this function samples 'M' random familyvectors and stores the indexes of those
+  * functions in each hashtableindex
+  */
 void SHGeneral::generate_hashtableindex()
 {
      cout<<"SHGeneral::generate_hashtableindex"<<endl;
@@ -57,6 +65,12 @@ void SHGeneral::generate_hashtableindex()
      cout<<"SHGeneral::generate_hashtableindex--END"<<endl;
 }
 
+/**
+  * Sample needsize points from size and fill it in result
+  * @param result       the array to hold the sample
+  * @param size         the total size of population
+  * @param needsize     sample size
+  */
 void SHGeneral::familysample(int result[], int size, int needsize)
 {
      vector<int> forchoose;
@@ -75,6 +89,9 @@ void SHGeneral::familysample(int result[], int size, int needsize)
 	 forchoose.clear();
 }
 
+/**
+  * Compute dotproduct of data with familyvector
+  */
 void SHGeneral::productcomputer()
 {
     cout<<"SHGeneral::productcomputer"<<endl;
@@ -82,15 +99,25 @@ void SHGeneral::productcomputer()
     {
         for(int j = 0; j < familysize; j++)
         {
-            dataproduct[i][j] = MyVector::dotproduct(D,data[i],familyvector[j]);
+            dataproduct[i][j] = MyVector::dotproduct(D, data[i], familyvector[j]);
         }
-        if(i%20000 == 0) {cout<<"SHGeneral::productcomputer"<<endl<<"current hashing data "<<i<<endl;
-        cout<<dataproduct[i][5]<<endl;}
+
+        if(i%20000 == 0)
+        {
+            cout << "computing product for " << i << endl;
+            // cout << dataproduct[i][5] << endl;
+        }
 
     }
     cout<<"SHGeneral::productcomputer--END"<<endl;
 }
 
+/**
+  * Computes the hashvalue for the the hashtables for a given data point
+  * @param product  dotproduct of data with all the familyvectors
+  * @param Rrank    FIXME: Radius ratio which can only be 0 in the current implementation
+  * @param table    Hashresults of data with all 'L' hashtables
+  */
 void SHGeneral::tableindex(float product[], int Rrank, unsigned int table[])
 {
     int familyint[familysize] = {};
@@ -111,4 +138,3 @@ void SHGeneral::tableindex(float product[], int Rrank, unsigned int table[])
         }
     }
 }
-
--- SHgeneral.h
+++ SHgeneral.h
@@ -8,35 +8,36 @@ class SHGeneral;
 #include "myvector.h"
 #include "myrandom.h"
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
 class SHGeneral{
     private:
-    float R[Alter];
-    float dataproduct[datasize][familysize];
-    float familyvector[familysize][D+1];
-    int hashtableindex[L][M];
-    unsigned int datahashresult[datasize][L];
-    int datahashtable[L][datasize];
-    int decision[datasize];
-    bool decisionavailable;
-    bool isinit;
+        float R[Alter];                                     // FIXME: Radius rations (predefined)
+        float dataproduct[datasize][familysize];            // dotproduct of data and familyvectors
+        float familyvector[familysize][D+1];                // vectors of the family
+        int hashtableindex[L][M];                           // Stores the index of familyvectors for each concatenation function
+        unsigned int datahashresult[datasize][L];           // hashresult: point -> table
+        int datahashtable[L][datasize];                     // Hashresult: table -> point
+        int decision[datasize];                             // FIXME: Always a zero vector in this implementation
+        bool decisionavailable;
+        bool isinit;
 
     public:
-    friend class SHSelection;
-    friend class SHIndex;
+        friend class SHSelection;
+        friend class SHIndex;
 
     public:
-    SHGeneral();
-    void init();
-    void tableindex(float [], int, unsigned int []);
+        SHGeneral();
+        void init();
+        void tableindex(float [], int, unsigned int []);
 
     private:
-    void family_generator();
-    void generate_hashtableindex();
-    void familysample(int result[], int size, int needsize);
-    void productcomputer();
+        void family_generator();
+        void generate_hashtableindex();
+        void familysample(int result[], int size, int needsize);
+        void productcomputer();
 };
 
 #endif // SHGENERAL_H_INCLUDED
